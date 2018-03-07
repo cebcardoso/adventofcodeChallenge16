4eb13ecdcd2b7f6ef288ece61d8ee5af1766d81b
cebcardoso <mrcarlogambini@gmail.com>
2018-03-07 23:37:19 +0200

    Completed

---
+++ index.php
@@ -0,0 +1,92 @@
+<?php
+
+class ChallengeSixteen {
+	private $inputArray;
+	private $programs;
+	const SPIN = 's';
+	const EXCHANGE = 'x';
+	const PARTNER = 'p';
+	
+	function __construct() {
+		$this->programs = 'abcdefghijklmnop';
+        $this->inputArray = explode(',', file_get_contents('input.txt'));
+    }
+	
+	public function getInputArray() {
+		return $this->inputArray;
+	}
+
+	public function getPrograms() {
+		return $this->programs;
+	}
+	
+	public function setPrograms($programs) {
+		$this->programs = $programs;
+	}
+
+	function pr($array) {
+		echo '<pre>';
+		print_r($array);
+		echo '</pre>';
+	}
+	
+	function performSpin($spin) {
+		$programsToChange = $this->getPrograms();
+		$newPrograms = '';
+		$count = 1;
+		
+		for ($k = 0; $k < strlen($programsToChange); $k++) {
+			$newPrograms[($k + $spin) % strlen($programsToChange)] = $programsToChange[$k];
+		}
+		
+		ksort($newPrograms);
+		
+		$this->setPrograms(implode($newPrograms));
+	}
+	
+	function performExchange($positions) {
+		$programsToChange = $this->getPrograms();
+		$position1Value = $programsToChange[$positions[0]];
+		$position2Value = $programsToChange[$positions[1]];
+		$programsToChange[$positions[0]] = $position2Value;
+		$programsToChange[$positions[1]] = $position1Value;
+		$this->setPrograms($programsToChange);
+	}
+	
+	function performPartner($programsPartner) {
+		$programsToChange = $this->getPrograms();
+		$position1Key = array_search($programsPartner[0], str_split($programsToChange));
+		$position2Key = array_search($programsPartner[1], str_split($programsToChange));
+		$position1KeyValue = $programsToChange[$position1Key];
+		$position2KeyValue = $programsToChange[$position2Key];
+		$programsToChange[$position1Key] = $position2KeyValue;
+		$programsToChange[$position2Key] = $position1KeyValue;
+		$this->setPrograms($programsToChange);
+	}
+	
+	function extractActions() {
+		foreach ($this->inputArray as $action) {
+			$actionType = $action[0];
+			$instruction = substr($action, 1, strlen($action) - 1);
+			
+			switch ($actionType) {
+				case self::SPIN:
+					$this->performSpin(1);
+					break;
+				case self::EXCHANGE:
+					$positions = explode("/", $instruction);
+					$this->performExchange($positions);
+					break;
+				case self::PARTNER:
+					$programsPartner = explode("/", $instruction);
+					$this->performPartner($programsPartner);
+					break;
+			}
+		}
+	}
+}
+
+	$challengeSixteen = new ChallengeSixteen();
+	$challengeSixteen->extractActions();
+	echo 'Final dance order: ' . $challengeSixteen->getPrograms();
+?>
